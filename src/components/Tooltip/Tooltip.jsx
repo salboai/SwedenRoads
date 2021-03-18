fcab1601b8b937926072f66ee3742360328444f8
Anders Gustafsson <andersgee@gmail.com>
2021-03-18 21:57:18 +0100

    typography in tooltip

--- src/components/Tooltip/Tooltip.jsx
+++ src/components/Tooltip/Tooltip.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { Typography } from "@material-ui/core";
 import "./tooltip.css";
 
 export default function Tooltip(props) {
   return (
     <div className="tooltip" key={props.key}>
       {props.children}
-      <span className="tooltiptext">tooltiptext here</span>
+      <span className="tooltiptext">
+        <Typography variant="body1" component="span">
+          tooltiptext here
+        </Typography>
+      </span>
     </div>
   );
 }
