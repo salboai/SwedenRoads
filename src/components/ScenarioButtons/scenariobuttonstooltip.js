c6f967332a7926d8baef7ecacb6ca319834a8516
andy <andersgee@gmail.com>
2021-03-22 15:06:41 +0100

    text

--- src/components/ScenarioButtons/scenariobuttonstooltip.js
+++ src/components/ScenarioButtons/scenariobuttonstooltip.js
@@ -3,10 +3,14 @@ const tooltiptext = {
   IndK2030: `Se hur tillståndet på vägarna förändras till 2030 med samma budgetanslag som idag för
   underhåll av vägbeläggning.`,
   IKls_2: `Se hur tillståndet på vägarna förändras om staten tilldelar två miljarder extra per år fram till
-  2030. 2 miljarder extra per år är vad som krävs för att inte försämra vägarna ytterligare fram till
+  2030.
+  
+  2 miljarder extra per år är vad som krävs för att inte försämra vägarna ytterligare fram till
   2030.`,
   IKls_3: `Se hur tillståndet på vägarna förändras om 4 miljarder extra per år tilldelas budgetanslaget för
-  vägbeläggning. Det är vad som krävs för att minimera andelen vägar i mycket dåligt skick.`,
+  vägbeläggning.
+  
+  Det är vad som krävs för att minimera andelen vägar i mycket dåligt skick.`,
 };
 
 export { tooltiptext };
