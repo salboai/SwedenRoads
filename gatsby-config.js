8b85a1f6ac0a789d66807eb99b3ea65ed42f4c3a
andy <andersgee@gmail.com>
2021-03-23 12:08:17 +0100

    test google-gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
     defaultTitle: `Våra Vägar`,
@@ -5,15 +9,31 @@ module.exports = {
     defaultDescription: `Väginformation, Karta och Underhållsbehov av Sveriges vägar.`,
     lang: `sv`,
     siteUrl: "https://www.varavagar.se",
-    defaultImage: "andyfx",
+    defaultImage: "transportföretagen_thumb",
     author: "Anders Gustafsson",
   },
   plugins: [
     "gatsby-plugin-top-layout",
     "gatsby-plugin-material-ui",
     "gatsby-plugin-react-helmet",
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [`${process.env.GTAG_MEASUREMENTID}`],
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_flags: "SameSite=None;Secure",
+          cookie_domain: "www.varavagar.se",
+          cookie_expires: 0,
+        },
+        pluginConfig: {
+          head: false,
+          //exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
+      },
+    },
   ],
   flags: {
     DEV_SSR: false,
-  }
+  },
 };
