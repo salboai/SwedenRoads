7d1d7b7a4f37456440eae3ecc07db5b561886b76
Kristin Eklöf <kristin@salbo.ai>
2021-04-06 10:30:43 +0200

    Update welcome text

--- src/components/TopNav/dialogdescriptions.js
+++ src/components/TopNav/dialogdescriptions.js
@@ -1,6 +1,6 @@
 const welcomecontent = `Välkommen till Transportföretagens Våra vägar! Här kan du se hur våra statliga
 vägar mår idag och hur de beräknas må år 2030 beroende på hur mycket
-budgetmedel som satsas.
+budgetmedel som satsas. Sidan innehåller en stor mängd data som kan ta en minut att ladda.
 
 Sverige behöver satsa mer på underhåll av vägarna. För bättre trafiksäkerhet, ökad
 konkurrenskraft och för att hela Sverige ska leva.`;
