baa3cafc476368a07bc8e57430381a8e46ec7cbb
andy <andersgee@gmail.com>
2021-04-29 12:45:57 +0200

    cookie optin in localStorage, meaning accept once for page and stay accepted for future visits

--- src/components/CookieBanner/CookieBanner.jsx
+++ src/components/CookieBanner/CookieBanner.jsx
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { GTAG_OPTIN_KEY } from "gatsby-plugin-google-gtag-optin"; // Or use your own if you changed it in the config
 import "./cookiebanner.css";
 
 export default function CookieBanner() {
+  const [accepted, setAccepted] = useState(
+    localStorage.getItem(GTAG_OPTIN_KEY)
+  );
+
   const handleConsent = () => {
     localStorage.setItem(GTAG_OPTIN_KEY, true);
+    setAccepted(true);
     if (typeof window.loadGtag == "function") {
       window.loadGtag();
     }
   };
 
-  return GTAG_OPTIN_KEY ? null : (
+  return accepted ? null : (
     <div className="cookiebanner">
-      <div>Sidan använder cookies för att se antal besökare.</div>
+      <div>Sidan använder cookies för besökarstatistik.</div>
       <button onClick={handleConsent} className="cookieacceptbutton">
         <div className="cookieacceptbuttontext">Acceptera</div>
       </button>
