f41948ab57c0f075cefacfdd31d7deb5b4c43f59
andy <andersgee@gmail.com>
2021-05-08 17:24:40 +0200

    loadGtag function outside component

--- src/components/CookieBanner/CookieBanner.jsx
+++ src/components/CookieBanner/CookieBanner.jsx
@@ -2,6 +2,14 @@ import React, { useState, useEffect } from "react";
 import { GTAG_OPTIN_KEY } from "gatsby-plugin-google-gtag-optin"; // Or use your own if you changed it in the config
 import "./cookiebanner.css";
 
+function loadGtag() {
+  const isBrowser = typeof window !== "undefined" && window;
+  if (isBrowser && typeof window.loadGtag == "function") {
+    console.log("loading Gtag");
+    window.loadGtag();
+  }
+}
+
 export default function CookieBanner() {
   const isBrowser = typeof window !== "undefined" && window;
   const [accepted, setAccepted] = useState(
@@ -11,16 +19,13 @@ export default function CookieBanner() {
   const handleConsent = () => {
     localStorage.setItem(GTAG_OPTIN_KEY, true);
     setAccepted(true);
-    if (typeof window.loadGtag == "function") {
-      window.loadGtag();
-    }
   };
 
   useEffect(() => {
     if (accepted) {
-      window.loadGtag();
+      loadGtag();
     }
-  }, []);
+  }, [accepted]);
 
   const ignoreComponent = accepted || !isBrowser;
   return ignoreComponent ? null : (
